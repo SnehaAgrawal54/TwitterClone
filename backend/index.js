97d6fdf99d770d44a48bb10d613fc181c4cfae55
Sneha Agrawal <snehagrawal121@gmail.com>
2024-05-24 11:35:46 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -6,26 +6,27 @@ import userRoute from "./routes/userRoute.js";
 import tweetRoute from "./routes/tweetRoute.js";
 import cors from "cors";
 
-dotenv.config({path:".env"})
+dotenv.config({ path: ".env" })
 
 connectDb()
 
-const app=express()
-const port=process.env.PORT
+const app = express()
+const port = process.env.PORT
 const corsOptions = {
-    origin:"http://localhost:3000",
-    credentials:true
+    origin: "http://localhost:3000",
+    methods: 'GET, PUT, POST, DELETE',
+    credentials: true
 }
 
 // middlewares
-app.use(express.urlencoded({extended:true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors(corsOptions));
 
 // api
-app.use("/api/v1/user",userRoute);
+app.use("/api/v1/user", userRoute);
 app.use("/api/v1/tweet", tweetRoute);
- 
 
-app.listen(port,()=>{console.log(`Server listening on port ${port}`);})
+
+app.listen(port, () => { console.log(`Server listening on port ${port}`); })
