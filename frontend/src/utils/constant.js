01ab0685045b43c2505fa2c031c0ab48c2dbcb24
SnehaAgrawal54 <161943505+SnehaAgrawal54@users.noreply.github.com>
2024-05-24 12:35:52 +0530

    Update constant.js

--- frontend/src/utils/constant.js
+++ frontend/src/utils/constant.js
@@ -1,3 +1,3 @@
-export const USER_API_END_POINT = "http://localhost:8080/api/v1/user";
-export const TWEET_API_END_POINT = "http://localhost:8080/api/v1/tweet";
+export const USER_API_END_POINT = "https://twitter-clone-lac-ten.vercel.app/api/v1/user";
+export const TWEET_API_END_POINT = "https://twitter-clone-lac-ten.vercel.app/api/v1/tweet";
 
